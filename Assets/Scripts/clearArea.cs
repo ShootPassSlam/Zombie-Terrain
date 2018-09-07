d0776571279d7afc9eb01102473b8a93839c1c8b
ShootPassSlam <dominicscotto@gmail.com>
2018-09-06 19:32:54 -0700

    first 3D terrain

---
+++ Assets/Scripts/clearArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clearArea : MonoBehaviour {
+
+	public float timeSinceLastTrigger = 0f;
+  // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeSinceLastTrigger += Time.deltaTime;
+
+    if(timeSinceLastTrigger > 1f){
+      Debug.Log("Found clear area");
+    }
+	}
+
+  void OnTriggerStay(){
+    timeSinceLastTrigger = 0;
+  }
+}
---
+++ Assets/Scripts/helicopter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helicopter : MonoBehaviour {
+  public AudioClip callSound;
+	
+  private bool called = false;
+  private AudioSource audioSource;
+  // Use this for initialization
+  void Start () {
+    audioSource = GetComponent<AudioSource>();
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    if(Input.GetButtonDown("Heli") && !called){
+      called = true;
+      //Play Sound
+      print("heli called");
+      audioSource.clip = callSound;
+      audioSource.Play();
+    }
+  }
+}
